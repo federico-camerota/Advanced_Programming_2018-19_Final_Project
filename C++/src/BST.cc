a2e0754df2d41236b68b3cfe50b54f50ce1d4dbb
Federico Camerota <fedes770@gmail.com>
2019-01-23 14:04:17 +0100

    Implemented insert function for BST

--- C++/src/BST.cc
+++ C++/src/BST.cc
@@ -39,10 +39,49 @@ void BST<K,V,Comp>::insert( const node_type& subtree){
     
     if (subtree == BST::nullnode) //check if target is a nullnode
 	return;
-    insert(subtree.data()); //copy data in target to the new tree
-    if (subtree.get_left())
-	insert(*subtree.get_left()); //copy left subtree
-    if (subtree.get_right())
-	insert(*subtree.get_right()); //copy right subtree
+    insert(subtree.data); //copy data in target to the new tree
+    if (subtree.left_child)
+	insert(*subtree.left_child); //copy left subtree
+    if (subtree.right_child)
+	insert(*subtree.right_child); //copy right subtree
 }
 
+template<class K, class V, class Comp>
+void BST<K,V,Comp>::insert(const key_type& key, const value_type& value){
+
+    if ((auto iter = find(key_type)) != end())
+	*iter.data = {key_type, value_type};
+    else{
+    
+	node_type *current_node{root.get()};
+	node_type *new_node{new node_type{key, value, nullptr}};
+	if (current_node == BST<K,V,Comp>::nullnode){ //check if the BST is empty 
+	
+	    current_node->parent=new_node; //set the new root node as parent of the nullnode
+	    new_node.right = (std::uniq_ptr<node_type>) root.release();//put the nullnode on the 
+								       //right child of the new node
+	    root = (std::uniq_ptr<node_type>) new_node;//put the new node in root
+	    return;
+	}
+	while (true){
+	
+	    auto& next_node{ (*current_node < *new_node) ? current_node->left_child : current_node->right_child };
+	    if (next_node == nullptr){ //found position in the tree for the new_node
+	    
+		next_node = (std::uniq_ptr<node_type>) new_node;//set new_node as child of 
+								 //current_node	
+		new_node->parent = current_node;//update parent member of new_node
+		return;
+	    }
+	    if (*next_node == BST<K,V,Comp>::nullnode){
+	    
+		new_node->right_child = next_node.release();//put the nullnode as new_node right
+							    //child
+		new_node->parent = current_node;//update new_node parent member
+		next_node = new_node;//put new_node as right child of current node
+		return;
+	    }
+	   current_node = new_node; 
+	}
+    }
+}
