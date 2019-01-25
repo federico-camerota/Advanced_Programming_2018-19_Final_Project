67ee0cf51dbd3c5484ab60590463eb2b49d1a44f
Federico Camerota <fedes770@gmail.com>
2019-01-25 16:04:26 +0100

    Implemented writeFile

--- python/exam_solution.py
+++ python/exam_solution.py
@@ -1,1 +1,33 @@
 # python/exam_solution.py
+
+class PostcardsList:
+
+    def __init__(self, file_name):
+        self._file = file_name
+        self._postcards = list()
+        self._date = dict()
+        self._from = dict()
+        self._to = dict()
+
+    def writeFile(self):
+
+        with open(self._file, 'w') as file_handler:
+            for  x in self._date:
+                file_handler.write(x + ' : ' + str(self._date[x]) + '\n')
+            for x in self._from:
+                file_handler.write(x + ' : ' + str(self._from[x]) + '\n')
+            for x in self._to:
+                file_handler.write(x + ' : ' + str(self._to[x]) + '\n')
+
+    def readFile(self, file_name = None):
+
+        if file_name == None:
+            file_name = self._file
+
+
+
+
+
+
+
+
